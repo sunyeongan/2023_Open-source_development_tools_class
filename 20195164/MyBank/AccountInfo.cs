a9d7d77fc4edecaf7199a7ac9f585d624d0e7db3
Donghyeon0313 <pdh9791@gmail.com>
2023-11-24 12:04:26 +0900

    Add: my file first upload #1

---
+++ 20195164/MyBank/AccountInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBank
+{
+    public class AccountInfo
+    {
+        public string AccountHolderName { get; }
+        public double Balance { get; }
+
+        public static void Main()
+        {
+            var account = new CheckingAccount("JohnDoe", 100.0);
+
+            account.Deposit(10.77);
+            account.Withdraw(50.0);
+        }
+    }
+}
