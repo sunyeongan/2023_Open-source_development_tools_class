1d51896b005c800f61fa729e488e49793a7ca801
SeokYeongeol <140517533+SeokYeongeol@users.noreply.github.com>
2023-11-24 12:05:31 +0900

    Add: my file first upload #1

---
+++ 20195183/Accounts/AccountInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounts
+{
+    public class AccountInfo
+    {
+        public AccountInfo(string name, double balance)
+        {
+            Console.WriteLine("{0} 님의 잔액은 {1}원 입니다.",name, balance);   
+        }
+    }
+}
---
+++ 20195183/Accounts/IAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounts
+{
+    public interface IAccount
+    {
+        double Balance { get; }
+        void Withdraw(double amount);
+        void Credit(double amount);
+
+    }
+}
