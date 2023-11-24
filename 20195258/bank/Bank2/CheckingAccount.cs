3a87dad2856f1843c19f6d95b316ff4711d2fcb6
JoInHo <zjxps2007@gmail.com>
2023-11-24 11:24:53 +0900

    Add Project

---
+++ 20195258/bank/Bank2/CheckingAccount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank2
+{
+    internal class CheckingAccount : IAccount
+    {
+        private decimal balance;
+        public void Deposit(decimal amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine($"Deposited: {amount:C}. New Balance: {balance:C}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid deposit amount.");
+            }
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount > 0 && balance >= amount)
+            {
+                balance -= amount;
+                Console.WriteLine($"Withdrawn: {amount:C}. New Balance: {balance:C}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid withdrawal amount or insufficient funds.");
+            }
+        }
+
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+
+       
+    }
+}
---
+++ 20195258/bank/Bank2/IAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank2
+{
+    internal interface IAccount
+    {
+        void Deposit(decimal amount);
+
+        void Withdraw(decimal amount);
+
+        decimal GetBalance();
+    }
+}
