e1a9cdc8d3bb487c484c5e6f79c386af3c0b08aa
Jinsung <wlstjd6524@naver.com>
2023-11-24 11:47:42 +0900

    Add: my file first upload #1

---
+++ 20195227/Accounts/AccountInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounts
+{
+    internal class AccountInfo
+    {
+        public static void Main(string[] args)
+        {
+            CheckingAccount bryan = new("Mr. Bryan Walton", 11.99);
+
+            bryan.Deposit(5.77);
+            bryan.WithDraw(11.22);
+            Console.WriteLine("Current balance is ${0}", bryan.Balance);
+        }
+    }
+}
