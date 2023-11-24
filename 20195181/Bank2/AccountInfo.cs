ae7558e97b4026828bb128989389b15bb37f7063
yadoran86 <yadoran0806@naver.com>
2023-11-24 12:13:35 +0900

    Add: my file first upload #1

---
+++ 20195181/Bank2/AccountInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank2
+{
+    public class AccountInfo
+    {
+        
+        public static void Main()
+        {
+            CheckingAccount ba = new CheckingAccount("Mr. Bryan Walton", 11.99);
+
+            ba.Withdraw(5.77);
+            ba.Deposit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
+        }
+    }
+
+}
---
+++ 20195181/Bank2/IAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank2
+{
+    public interface IAccount
+    {
+        void Withdraw(double amount);
+        void Deposit(double amount);
+        
+    }
+}
