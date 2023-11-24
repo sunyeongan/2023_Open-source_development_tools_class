10e9a8f190ab2dd51a106ebbbe15b6b54464582b
parkseokju <qkrtjrwn98@naver.com>
2023-11-24 12:07:16 +0900

    Add my file first upload #1

---
+++ 20205171/bank/BankTest/BankAccountTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BankTest
+{
+    [TestClass]
+    public class BankAccountTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            account.Debit(debitAmount);
+
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+    }
+}
---
+++ 20205171/bank/UnitTestProject1/BankAccountTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BankAccountNS;
+
+namespace BankTest
+{
+    [TestClass]
+    public class BankAccountTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double beginningBalance = 11.99;
+            double debitAmount = 13.99;
+            double expected = -2.00; //exception 발생
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            account.Debit(debitAmount);
+
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            double beginningBalance = 11.99;
+            double debitAmount = -2.00; //exception 발생
+            double expected = 13.99;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            account.Debit(debitAmount);
+
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            account.Debit(debitAmount);
+
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+    }
+}
