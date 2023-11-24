e387976a8c58b1e7a359ec7a03be44c1de0c48b8
C-JS <106006746+ChoiJunsoo@users.noreply.github.com>
2023-11-24 12:35:16 +0900

    Add: my file first upload #1

---
+++ 20193844/Accounts/AccountInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounts
+{
+    internal class AccountInfo
+    {
+        public satic void Main()
+        {
+            CheckingAccount bryan = new("Mr. Bryan Walton", 11.99);
+
+            bryan.Deposit(5.77);
+            bryan.Withdraw(11.22);
+            Console.WriteLine("Current balance is ${0}", bryan.Balance);
+        }
+
+    }
+}
