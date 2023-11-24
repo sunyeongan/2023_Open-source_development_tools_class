a2b0a81093165b7117cb46b18624d38a973bdcba
choijimin <jiminchoi1221@gmail.com>
2023-11-24 12:05:49 +0900

    Add: my file first upload #1

---
+++ 20215263/Accounts/AccountInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounts
+{
+    public class AccountInfo
+    {
+        private readonly string m_customerName;
+        private double m_balance; 
+        private AccountInfo() { }
+
+        public AccountInfo(string customerName, double balance)
+        {
+            m_customerName = customerName;
+            m_balance = balance;
+        }
+
+        public string CustomerName
+        {
+            get { return m_customerName; }
+        }
+        public double Balance
+        {
+            get { return m_balance; }
+        }
+
+        public static void Main()
+        {
+            CheckingAccount bryan = new CheckingAccount("Mr. Bryan Walton", 11.99);
+        }
+    }
+}
---
+++ 20215263/AccountsTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AccountsTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AccountsTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ff5246ef-454b-474c-9950-f51068b716f1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
