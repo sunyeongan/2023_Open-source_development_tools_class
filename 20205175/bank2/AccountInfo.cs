83075dab31f2220e56d450f20ed7342a41268b4d
Junseo11 <0302ser@naver.com>
2023-11-24 11:40:54 +0900

    c

---
+++ 20205175/bank2/AccountInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bank2;
+
+namespace bank
+{
+    public class AccountInfo
+    {
+
+        
+        public static void Main()
+        {
+            CheckingAccount bryan = new CheckingAccount("Mr. Bryan Walton", 11.99);
+
+            bryan.Deposit(5.77);
+            bryan.Withdraw(11.22);
+            Console.WriteLine("current balance is ${0}", bryan.Balance);
+
+        }
+    }
+
+
+}
