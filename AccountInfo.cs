b45894f9e4f41fe769a46d5e91d83607a8228eff
gjwjddbs <yeoun0603@naver.com>
2023-11-28 11:22:00 +0900

    my file first upload #1

---
+++ AccountInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank2
+{
+    public class AccountInfo
+    {
+        public string m_customername; //고객 이름
+        public double m_balance=1000.0; //잔액
+
+        public static void Main(string[] args)
+        {
+            CheckingAccount bryan = new CheckingAccount("Mr.Bryan Waltion", 11.99);
+            bryan.Deposit(5.77);
+            bryan.Withdraw(300.0);
+            Console.WriteLine("계좌의 잔액: " + bryan.m_balance);
+
+        }
+    }
+
+}
