e29b53647e90fbb6187d8618f856453476ff7a87
pillwood <144303492+pillwood@users.noreply.github.com>
2023-12-01 09:30:53 +0900

    Add files via upload

---
+++ 20205276/CheckingAccountTests.cs
@@ -0,0 +1,36 @@
+using Accounts;
+
+namespace AccountTests
+{
+    [TestClass]
+    public class CheckingAccountTests
+    {
+        [TestMethod]
+        [Timeout(2000)]
+        public void Withdraw_ValidAmount_ChangesBalance()
+        {
+            // arrange
+            double currentBalance = 10.0;
+            double withdrawal = 1.0;
+            double expected = 9.0;
+            var account = new CheckingAccount("JohnDoe", currentBalance);
+
+            // act
+            account.Withdraw(withdrawal);
+
+            // assert
+            Assert.AreEqual(expected, account.balance);
+        }
+
+        [TestMethod]
+        [Timeout(2000)]
+        public void Withdraw_AmountMoreThanBalance_Throws()
+        {
+            // arrange
+            var account = new CheckingAccount("John Doe", 10.0);
+
+            // act and assert
+            Assert.ThrowsException<System.ArgumentException>(() => account.Withdraw(20.0));
+        }
+    }
+}
